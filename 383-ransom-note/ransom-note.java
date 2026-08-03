3dfe56d8a5eebcaea256cfc179b6e836dffeebe8
RajaNamdeo13 <143259939+RajaNamdeo13@users.noreply.github.com>
2026-08-04 00:00:17 +0530

    Time: 1 ms (99.97%) | Memory: 46.2 MB (89.95%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] count = new int[26];
+
+        // Count characters in magazine
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        // Use characters for ransom note
+        for (char c : ransomNote.toCharArray()) {
+
+            count[c - 'a']--;
+
+            if (count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+        
+    
